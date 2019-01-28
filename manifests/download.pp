a4327c3f9a8ac720be421d6aaa7272e95aabadb0
root <dekormag@gmail.com>
2019-01-28 23:08:40 +0000

    change class include

--- manifests/download.pp
+++ manifests/download.pp
@@ -5,13 +5,14 @@ class mattermost::download (
   String  $mmdir          = $mattermost::params::mmdir,
   String  $mmdata         = $mattermost::params::mmdata,
   String  $mmdownload     = $mattermost::params::mmdownload,
+  String  $mmchecksum     = $mattermost::params::mmchecksum,
   ) inherits mattermost::params {
 
   archive {"${mmdir}/mattermost-team-${mmversion}-linux-amd64.tar.gz":
     ensure        => present,
     extract       => true,
     source        => $mmdownload,
-    checksum      => '1220e25f501e41db9bee6af14dd12583c1f95658a55862da42a1268d160b10c4',
+    checksum      => $mmchecksum,
     checksum_type => 'sha256',
     creates       => '/opt/mattermost',
   }
