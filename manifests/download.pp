277d4fec1ba7a0ca18489c7ad9fa45326300d767
root <dekormag@gmail.com>
2019-01-29 15:50:42 +0000

    change checksum var4

--- manifests/download.pp
+++ manifests/download.pp
@@ -5,18 +5,16 @@ class mattermost::download (
   String  $mmdir          = $mattermost::params::mmdir,
   String  $mmdata         = $mattermost::params::mmdata,
   String  $mmdownload     = $mattermost::params::mmdownload,
-  $mmchecksum             = $mattermost::params::mmchecksum,
+          $mmchecksum     = $mattermost::params::mmchecksum,
   ) inherits mattermost::params {
 
   archive {"${mmdir}/mattermost-team-${mmversion}-linux-amd64.tar.gz":
-    ensure        => present,
-    extract       => true,
-    extract_path  => $mmdir,
-    source        => "$mmdownload",
-    checksum      => "$mmchecksum",
-#    checksum_type => 'sha256',
-    creates       => '/opt/mattermost',
-    cleanup       => true,
+    ensure       => present,
+    extract      => true,
+    extract_path => $mmdir,
+    source       => $mmdownload,
+    checksum     => $mmchecksum,
+    creates      => '/opt/mattermost',
+    cleanup      => true,
   }
-  notify {"$mmchecksum":}
 }
