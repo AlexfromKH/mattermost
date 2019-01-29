04496c0ac0121681435837bfe4dd610cda7a7b98
root <dekormag@gmail.com>
2019-01-29 20:40:51 +0000

    fix mmdata in setup.pp

--- manifests/setup.pp
+++ manifests/setup.pp
@@ -4,25 +4,26 @@
 class mattermost::setup (
   String $mmdir       = $mattermost::params::mmdir,
   String $mmuser      = $mattermost::params::mmuser,
+  String $mmdata      = $mattermost::params::mmdata,
 ) inherits mattermost::params {
 
-  file {$mmdir:
+  file { $mmdata:
     ensure  => directory,
   }
   group { $mmuser:
     ensure  => present,
   }
-  user {$mmuser:
+  user { $mmuser:
     name    => $mmuser,
     groups  => $mmuser,
     require => Group[$mmuser],
   }
-#  file {"${mmdir}/${mmuser}":
-#    ensure  => 'directory',
-#    group   => $mmuser,
-#    mode    => '0664',
-#    owner   => $mmuser,
-#    require => [ User[$mmuser], Group[$mmuser] ],
-#  }
+  file { "${mmdir}/${mmuser}":
+    ensure  => 'directory',
+    group   => $mmuser,
+    mode    => '0664',
+    owner   => $mmuser,
+    require => [ User[$mmuser], Group[$mmuser] ],
+  }
   notify {"${mmdir}/${mmuser}":}
 }
